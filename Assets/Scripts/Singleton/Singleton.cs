1673b9e222330984f47ea5191e34de45ca6e9d27
Peter Eichinger <peter.eichinger@gmail.com>
2015-11-09 09:33:41 +0100

    Add singleton implementation

---
+++ Assets/Scripts/Singleton/Singleton.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+/// <summary>Singleton pattern. Should only be used by <see cref="Toolbox"/>.</summary>
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour {
+	private static T _instance;
+
+	private static object _lock = new object();
+
+	public static T Instance {
+		get {
+			lock (_lock) {
+				if (_instance == null) {
+					_instance = (T)FindObjectOfType(typeof(T));
+
+					if (FindObjectsOfType(typeof(T)).Length > 1) {
+						Debug.LogError("[Singleton] Something went really wrong " +
+							" - there should never be more than 1 singleton!" +
+							" Reopening the scene might fix it.");
+						return _instance;
+					}
+
+					if (_instance == null) {
+						GameObject singleton = new GameObject();
+						_instance = singleton.AddComponent<T>();
+						singleton.name = "(singleton) " + typeof(T).ToString();
+
+						DontDestroyOnLoad(singleton);
+
+						Debug.Log("[Singleton] An instance of " + typeof(T) +
+							" is needed in the scene, so '" + singleton +
+							"' was created with DontDestroyOnLoad.");
+					} else {
+						Debug.Log("[Singleton] Using instance already created: " +
+							_instance.gameObject.name);
+					}
+				}
+
+				return _instance;
+			}
+		}
+	}
+}
