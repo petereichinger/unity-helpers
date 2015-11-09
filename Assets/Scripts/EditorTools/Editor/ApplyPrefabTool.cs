ca04cc9627ed812b4e505d708f56dd3a1d563516
Peter Eichinger <peter.eichinger@gmail.com>
2015-11-09 10:25:55 +0100

    Add Apply Prefab Tool

---
+++ Assets/Scripts/EditorTools/Editor/ApplyPrefabTool.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityHelpers.EditorTools.Editor {
+
+	/// <summary>Apply the changes to all selected prefabs.</summary>
+	public class ApplyPrefabTool {
+
+		[MenuItem("Tools/Apply Selected Prefabs")]
+		private static void ApplySelectedPrefabs() {
+			foreach (GameObject go in Selection.gameObjects) {
+				if (!go) {
+					continue;
+				}
+				var instanceRoot = PrefabUtility.FindRootGameObjectWithSameParentPrefab(go);
+				if (!instanceRoot) {
+					continue;
+				}
+				var targetPrefab = PrefabUtility.GetPrefabParent(instanceRoot);
+				if (!targetPrefab) {
+					continue;
+				}
+				Undo.RecordObject(instanceRoot, "Apply prefab");
+				Undo.RecordObject(targetPrefab, "Apply prefab");
+				Undo.FlushUndoRecordObjects();
+				PrefabUtility.ReplacePrefab(instanceRoot, targetPrefab,
+					ReplacePrefabOptions.ConnectToPrefab);
+			}
+		}
+	}
+}
